f792838f219bb5a3ec6ef02158731d8938143cf7
pbandierapaiva <paiva@unifesp.br>
2021-03-09 13:39:06 -0300

    pacote express.js instalado

--- dtgsrv.js
+++ dtgsrv.js
@@ -6,7 +6,7 @@ const port = 8793          //process.env.PORT
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World!\n')
+  res.end('Alo mundo DTG!\n')
 })
 
 server.listen(port, hostname, () => {
