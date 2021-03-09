1303dac50ee3a9de5169b42dec86f0080389b7df
pbandierapaiva <paiva@unifesp.br>
2021-03-09 14:05:51 -0300

    alterado dtgsrv.js para usar expressJS, acertando detalhes - a porta

--- dtgsrv.js
+++ dtgsrv.js
@@ -1,24 +1,10 @@
-// Aplicação sem expressJS
-//const http = require('http')
-
-//const hostname = '127.0.0.1'
-//const port = 8793          //process.env.PORT
-
-//const server = http.createServer((req, res) => {
-//  res.statusCode = 200
-//  res.setHeader('Content-Type', 'text/plain')
-//  res.end('Alo mundo DTG!\n')
-//})
-
-//server.listen(port, hostname, () => {
-//  console.log(`Server running at http://${hostname}:${port}/`)
-//})
-
+// Protótipo
 
 var express = require('express');
 var app = express();
+const port = 8793
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.send('Alo mundo DTG');
 });
