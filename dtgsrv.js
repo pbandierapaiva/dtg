254b320a7bd8095554a8766bf1703d0c79d65b01
pbandierapaiva <paiva@unifesp.br>
2021-03-09 14:56:07 -0300

    colocando mecanismo de deployment para o servidor

--- dtgsrv.js
+++ dtgsrv.js
@@ -4,11 +4,19 @@ var express = require('express');
 var app = express();
 const port = 8793
 
-// respond with "hello world" when a GET request is made to the homepage
+// Para deploy da aplicação no servidor
+const simpleGit = require('simple-git');
+const git = simpleGit();   
+
+app.get('/deploy', function(req, res) {
+	git.pull()
+	res.send('dir atualizado');
+});
+
+// Alo mundo
 app.get('/', function(req, res) {
   res.send('Alo mundo DTG');
 });
-
 
 app.listen(port, () => {
   console.log(`DTG server em http://localhost:${port}`)
