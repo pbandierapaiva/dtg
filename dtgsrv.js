cae70cb12a7d56ba50a4d177c0f3e803fe1c30f7
tymonsx <tymonsx@hotmail.comm>
2021-03-10 10:43:01 -0300

    teste capa

--- dtgsrv.js
+++ dtgsrv.js
@@ -22,7 +22,7 @@ app.get('/pmstatus', function(req, res) {
 });
 // Alo mundo
 app.get('/', function(req, res) {
-  res.send('Alo mundo!!!<br/> <b>Servidor DTG </b> alterado ');
+  res.send('Alo mundo!!!<br/> <b>Servidor DTG </b> alterado Capa');
 });
 
 app.listen(port, () => {
