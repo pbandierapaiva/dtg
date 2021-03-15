9c0c9f1d1b9a72bdc8afafe5d56eb9febead8e2e
tymonsx <tymonsx@hotmail.comm>
2021-03-15 10:36:12 -0300

    novo teste maridb

--- teste_mdb.js
+++ teste_mdb.js
@@ -12,10 +12,10 @@ async function asyncFunction() {
   let conn;
   try {
 	conn = await pool.getConnection();
-	const rows = await conn.query("SELECT 1 as val");
+	const rows = await conn.query("SELECT * from histo_ntg");
 	console.log(rows); //[ {val: 1}, meta: ... ]
-	const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-	console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+	//const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+	//console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
 
   } catch (err) {
 	throw err;
