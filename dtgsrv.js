97642f90ea2db144ffdb11a5c684b7693223a7b7
tymonsx <tymonsx@hotmail.comm>
2021-04-15 11:35:08 -0300

    teste de login

--- dtgsrv.js
+++ dtgsrv.js
@@ -25,6 +25,35 @@ app.get('/', function(req, res) {
   res.send('Alo mundo!!!<br/> <b>Servidor DTG </b> alterado Capa2');
 });
 
+//login
+app.post('/auth/login', (req, res) => {
+	//receber login e senha
+	console.log("login endpoint called; request body:");	
+	console.log(req.body);	
+	const { login, senha } = req.body;	
+	console.log("Login:" + login);
+	console.log("senha:" + senha);
+	//critografar em md5
+	//fazer select no banco de dados
+	//criar o token de acesso
+	//retornar o usuario e o token
+	/*
+  if (isAuthenticated({email, senha}) === false) {
+    const status = 401
+    const message = 'E-mail ou senha incorretos!'
+    res.status(status).json({status, message})
+    return
+  }
+  const access_token = createToken({email, senha})
+  let user = { ...userdb.usuarios.find(user => user.email === email && user.senha === senha) }
+  delete user.senha
+  console.log("Access Token:" + access_token);
+  console.log("User:" + user);
+  res.status(200).json({access_token, user})
+  */
+})
+
+
 app.listen(port, () => {
   console.log(`DTG server em http://localhost:${port}`)
 })
