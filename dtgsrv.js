a063839399df137e1ea525a9dd010f1ef5683a4d
tymonsx <tymonsx@hotmail.comm>
2021-07-07 10:10:32 -0300

    webservice incluir_hcg

--- dtgsrv.js
+++ dtgsrv.js
@@ -2388,46 +2388,40 @@ app.post("/ativa_inativar_instituicao", jsonParser, async (req, res) => {
     return;
   }
 });
+*/
 
-//################################## tela de Cadastro de Instituição #########################
-//webservice de incluir Instituição
-app.post("/incluir_instituicao", jsonParser, async (req, res) => {
+
+//################################## tela de Cadastro de hCG #########################
+//webservice de incluir hCG
+app.post("/incluir_hcg", jsonParser, async (req, res) => {
   //receber dados para inclusão
   let {
-    nome,
-    cep,
-    logradouro,
-    numero,
-    complemento,
-    uf,
-    cidade,
-    bairro,
+    data_hcg,
+    result_hcg,
+    id_imagem,
+    cadastrante
   } = req.body;
   //definir o sql padrão
   let sql =
-    "insert into instituicao ( " +
-    " nome_inst , " +
-    " logradouro_inst ," +
-    " num_inst ," +
-    " cep_inst , " +
-    " bairro_inst , " +
-    " cidade_inst , " +
-    " uf_inst , " +
-    " complemento_inst  " +
-    ") values(?,?,?,?,?,?,?,?)";
-  let values = [nome, logradouro, numero, cep, bairro, cidade, uf, complemento];
+    "insert into hcg ( " +
+    " data_hcg, " +
+    " result_hcg, " +
+    " id_imagem, " +
+    " cadastrante " +
+    " ) values (?,?,?,?)";
+  let values = [data_hcg, result_hcg, id_imagem, cadastrante];
   let resultado = await insert_mdb(sql, values);
   if (resultado.affectedRows > 0) {
     res.status(200).json({ resultado: [values, resultado.insertId] });
     return;
   } else {
     const status = 409;
-    const message = "Não foi possível incluir os dados da Instituição.";
+    const message = "Não foi possível incluir os dados do hCG.";
     res.status(status).json({ status, message });
     return;
   }
 });
-
+/*
 //webservice de carrega dados de um instituição
 app.post("/dados_instituicao", jsonParser, async (req, res) => {
   //receber descricao
